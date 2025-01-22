4802f5f361cde2bcdc8dd37225ef702b2f6458d3
plunkydev <plunkydesign@gmail.com>
2025-01-22 09:47:27 -0500

    Entorno de desarrollo creado

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css'
+const mainContainer = document.querySelector('.mainContainer')
+const h1 = document.createElement('h1')
+h1.textContent = 'Hello Plunky!'
+mainContainer.appendChild(h1)
+console.log('Hola plunky!')
