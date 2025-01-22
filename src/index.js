ffc02c98490491192a29c943fa24d3a8a75e57b1
plunkydev <plunkydesign@gmail.com>
2025-01-22 10:02:55 -0500

    Prueba de script 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css'
 const mainContainer = document.querySelector('.mainContainer')
 const h1 = document.createElement('h1')
-h1.textContent = 'Hello Plunky!'
+h1.textContent = 'Hello Plunky otra vez!'
 mainContainer.appendChild(h1)
 console.log('Hola plunky!')
