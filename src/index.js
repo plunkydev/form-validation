2851ab78e8dbc7bb2a2be84fc4322089eab4c01d
plunkydev <plunkydesign@gmail.com>
2025-01-22 12:21:36 -0500

    Importacion de createForm para en mainContainer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style.css'
+import { createForm } from './components/renderForm.js'
+
 const mainContainer = document.querySelector('.mainContainer')
-const h1 = document.createElement('h1')
-h1.textContent = 'Hello Plunky otra vez!'
-mainContainer.appendChild(h1)
-console.log('Hola plunky!')
+mainContainer.appendChild(createForm())
