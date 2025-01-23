b0eaffe02c6608b847f56a6ad565ce4f80342249
plunkydev <plunkydesign@gmail.com>
2025-01-23 14:51:28 -0500

    Refactorización de la función  para incluir validación en tiempo real: agregado soporte para validación en tiempo real de campos (email, país, código postal, contraseña y confirmación de contraseña), incluidos mensajes de error dinámicos para cada campo, incorporadas opciones por defecto en el campo de país para mejorar la usabilidad, reorganización de atributos en el formulario y campos para cumplir con las mejores prácticas de accesibilidad, añadidos listeners para manejar eventos de entrada y cambio en los campos, e importadas funciones de validación desde . Esta actualización mejora la experiencia del usuario y asegura que los datos enviados cumplan con los requisitos del formulario.

--- src/components/renderForm.js
+++ src/components/renderForm.js
@@ -1,3 +1,5 @@
+import { validateEmail, validateOptionInputs, validatePostalCode, validatePassword, validatePasswordConfirm } from '../validate/formValidate.js'
+
 function createForm () {
   // Crear el formulario
   const form = document.createElement('form')
@@ -5,6 +7,8 @@ function createForm () {
   form.className = 'form'
   form.setAttribute('name', 'registrationForm')
   form.setAttribute('method', 'post')
+  form.setAttribute('action', '#')
+  form.setAttribute('novalidate', true)
 
   // Input para el email
   const emailLabel = document.createElement('label')
@@ -13,13 +17,19 @@ function createForm () {
 
   const emailInput = document.createElement('input')
   emailInput.id = 'email'
-  emailInput.className = 'input email'
+  emailInput.className = 'email'
   emailInput.setAttribute('type', 'email')
   emailInput.setAttribute('name', 'email')
   emailInput.setAttribute('required', true)
 
+  const emailMessage = document.createElement('span')
+  emailMessage.id = 'emailMessage'
+  emailMessage.className = 'message'
+  emailMessage.textContent = 'Mensaje'
+
   form.appendChild(emailLabel)
   form.appendChild(emailInput)
+  form.appendChild(emailMessage)
 
   // Fieldset para país y código postal
   const fieldsetCountry = document.createElement('fieldset')
@@ -34,16 +44,30 @@ function createForm () {
 
   const countrySelect = document.createElement('select')
   countrySelect.id = 'country'
-  countrySelect.className = 'input country'
+  countrySelect.className = 'country'
   countrySelect.setAttribute('name', 'country')
-  countrySelect.setAttribute('required', true);
+  countrySelect.setAttribute('required', true)
 
+  // Agregar una opción por defecto como placeholder
+  const placeholderOption = document.createElement('option')
+  placeholderOption.setAttribute('value', '')
+  placeholderOption.setAttribute('disabled', true)
+  placeholderOption.setAttribute('selected', true)
+  placeholderOption.textContent = 'Seleccione un país'
+  countrySelect.appendChild(placeholderOption);
+
+  // Agregar las opciones de países
   ['Colombia', 'Venezuela', 'Ecuador', 'Perú', 'Chile'].forEach(country => {
     const option = document.createElement('option')
     option.setAttribute('value', country.toLowerCase())
     option.textContent = country
     countrySelect.appendChild(option)
   })
+
+  const countryMessage = document.createElement('span')
+  countryMessage.id = 'countryMessage'
+  countryMessage.className = 'message'
+  countryMessage.textContent = 'Debe seleccionar un país'
 
   // Input para el código postal
   const postalLabel = document.createElement('label')
@@ -52,16 +76,24 @@ function createForm () {
 
   const postalInput = document.createElement('input')
   postalInput.id = 'postalCode'
-  postalInput.className = 'input postal-code'
+  postalInput.className = 'postalCode'
   postalInput.setAttribute('type', 'text')
   postalInput.setAttribute('name', 'postalCode')
   postalInput.setAttribute('required', true)
-  postalInput.setAttribute('pattern', '\\d{4,6}') // Validar entre 4 y 6 dígitos
+  postalInput.setAttribute('minlength', '4')
+  postalInput.setAttribute('maxlength', '6')
+
+  const postalMessage = document.createElement('span')
+  postalMessage.id = 'postalMessage'
+  postalMessage.className = 'message'
+  postalMessage.textContent = 'Mensaje'
 
   fieldsetCountry.appendChild(countryLabel)
   fieldsetCountry.appendChild(countrySelect)
+  fieldsetCountry.appendChild(countryMessage)
   fieldsetCountry.appendChild(postalLabel)
   fieldsetCountry.appendChild(postalInput)
+  fieldsetCountry.appendChild(postalMessage)
   form.appendChild(fieldsetCountry)
 
   // Fieldset para contraseña
@@ -77,11 +109,16 @@ function createForm () {
 
   const passwordInput = document.createElement('input')
   passwordInput.id = 'password'
-  passwordInput.className = 'input password'
+  passwordInput.className = 'password'
   passwordInput.setAttribute('type', 'password')
   passwordInput.setAttribute('name', 'password')
   passwordInput.setAttribute('required', true)
   passwordInput.setAttribute('minlength', '8')
+
+  const passwordMessage = document.createElement('span')
+  passwordMessage.id = 'passwordMessage'
+  passwordMessage.className = 'message'
+  passwordMessage.textContent = 'La contraseña debe tener mayusculas, minusculas, numeros y al menos 8 caracteres'
 
   // Input para confirmar contraseña
   const confirmLabel = document.createElement('label')
@@ -96,10 +133,16 @@ function createForm () {
   confirmInput.setAttribute('required', true)
   confirmInput.setAttribute('minlength', '8')
 
+  const confirmMessage = document.createElement('span')
+  confirmMessage.id = 'confirmMessage'
+  confirmMessage.className = 'message'
+
   fieldsetPassword.appendChild(passwordLabel)
   fieldsetPassword.appendChild(passwordInput)
+  fieldsetPassword.appendChild(passwordMessage)
   fieldsetPassword.appendChild(confirmLabel)
   fieldsetPassword.appendChild(confirmInput)
+  fieldsetPassword.appendChild(confirmMessage)
   form.appendChild(fieldsetPassword)
 
   // Botón de enviar
@@ -109,10 +152,40 @@ function createForm () {
   submitButton.textContent = 'Enviar'
   form.appendChild(submitButton)
 
-  // Agregar el formulario al cuerpo del documento
+  // Agregar validación en tiempo real-------------------------------------------------------------------------------------------------------------
+
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+  })
+
+  emailInput.addEventListener('input', () => {
+    validateEmail(emailInput, emailMessage, 'email')
+  })
+
+  function validateCountrySelection () {
+    validateOptionInputs(countrySelect, countryMessage, 'country')
+  }
+
+  // Validación al cambiar la selección
+  countrySelect.addEventListener('change', validateCountrySelection)
+
+  // Validación al salir del campo si no se seleccionó nada
+  countrySelect.addEventListener('blur', validateCountrySelection)
+
+  passwordInput.addEventListener('input', () => {
+    validatePassword(passwordInput, passwordMessage, 'password')
+  })
+
+  confirmInput.addEventListener('input', () => {
+    validatePasswordConfirm(confirmInput, passwordInput, confirmMessage, 'confirm-password')
+  })
+
+  postalInput.addEventListener('input', () => {
+    validatePostalCode(postalInput, postalMessage, 'postalCode')
+  })
+
+  // Retornar el formulario
   return form
 }
-
-// Llamar a la función para crear el formulario
 
 export { createForm }
