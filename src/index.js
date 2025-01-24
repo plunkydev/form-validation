1b444e22ca0d7899ee8507ed85b34052ccc157d1
plunkydev <plunkydesign@gmail.com>
2025-01-24 16:36:18 -0500

    Integrado formFloatMessage al proyecto para gestionar mensajes flotantes y actualizar el cuerpo del documento con su implementación inicial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './style.css'
 import { createForm } from './components/renderForm.js'
+import { formFloatMessage } from './components/formFloatMessage.js'
 
 const mainContainer = document.querySelector('.mainContainer')
+const body = document.querySelector('body')
+body.appendChild(formFloatMessage('', '', 'none'))
 mainContainer.appendChild(createForm())
